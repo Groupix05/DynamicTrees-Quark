64cb2f8f1ab5f2dbf0c7ee990211bd7245cb78b0
Max Hyper <massimocantugarcia@gmail.com>
2023-06-11 19:45:31 -0300

    finished dynamic glowshroom (no worldgen yet)

--- src/main/java/maxhyper/dtquark/DTQuarkRegistries.java
+++ src/main/java/maxhyper/dtquark/DTQuarkRegistries.java
@@ -1,22 +1,14 @@
 package maxhyper.dtquark;
 
-import com.ferreusveritas.dynamictrees.DynamicTrees;
 import com.ferreusveritas.dynamictrees.api.registry.TypeRegistryEvent;
-import com.ferreusveritas.dynamictrees.api.worldgen.FeatureCanceller;
 import com.ferreusveritas.dynamictrees.block.leaves.LeavesProperties;
 import com.ferreusveritas.dynamictrees.util.CommonVoxelShapes;
-import com.ferreusveritas.dynamictrees.worldgen.featurecancellation.MushroomFeatureCanceller;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.configurations.HugeMushroomFeatureConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.RandomBooleanFeatureConfiguration;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-
-import java.util.Set;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DTQuarkRegistries {
@@ -24,31 +16,16 @@ public class DTQuarkRegistries {
     public static final VoxelShape SHROOM_AGE0 = Shapes.create(0,0,0,1,0.75,1);
     public static final VoxelShape MUSHROOM_CAP_SHORT_ROUND = Block.box(5D, 3D, 5D, 11D, 7D, 11D);
     public static final VoxelShape ROUND_SHORT_MUSHROOM = Shapes.or(CommonVoxelShapes.MUSHROOM_STEM, MUSHROOM_CAP_SHORT_ROUND);
+
+    public static void setup (){
+        CommonVoxelShapes.SHAPES.put(new ResourceLocation(DynamicTreesQuark.MOD_ID,"glow_shroom_age0").toString(), SHROOM_AGE0);
+        CommonVoxelShapes.SHAPES.put(new ResourceLocation(DynamicTreesQuark.MOD_ID,"round_short_mushroom").toString(), ROUND_SHORT_MUSHROOM);
+    }
+
     @SubscribeEvent
     public static void registerLeavesPropertiesTypes(final TypeRegistryEvent<LeavesProperties> event) {
         event.registerType(new ResourceLocation(DynamicTreesQuark.MOD_ID, "blossom"), BlossomLeavesProperties.TYPE);
     }
 
-    public static void setup (){
-        CommonVoxelShapes.SHAPES.put(DynamicTreesQuark.MOD_ID+":glow_shroom_age0", SHROOM_AGE0);
-        CommonVoxelShapes.SHAPES.put(DynamicTreesQuark.MOD_ID+":round_short_mushroom", ROUND_SHORT_MUSHROOM);
-    }
-
-    public static final FeatureCanceller MUSHROOM_CANCELLER = new MushroomFeatureCanceller<>(DynamicTreesQuark.location("mushroom"), HugeMushroomFeatureConfiguration.class){
-        @Override
-        public boolean shouldCancel(ConfiguredFeature<?, ?> configuredFeature, Set<String> namespaces) {
-            final ResourceLocation featureName = configuredFeature.feature().getRegistryName();
-
-            if (featureName == null) return false;
-
-            return featureName.getNamespace().equals("quark") && featureName.getPath().equals("glow_shrooms");
-        }
-    };
-
-    @SubscribeEvent
-    public static void onFeatureCancellerRegistry(final com.ferreusveritas.dynamictrees.api.registry.RegistryEvent<FeatureCanceller> event) {
-
-        event.getRegistry().registerAll(MUSHROOM_CANCELLER);
-    }
 
 }
--- src/main/java/maxhyper/dtquark/DynamicTreesQuark.java
+++ src/main/java/maxhyper/dtquark/DynamicTreesQuark.java
@@ -4,33 +4,21 @@ import com.ferreusveritas.dynamictrees.api.GatherDataHelper;
 import com.ferreusveritas.dynamictrees.api.registry.RegistryHandler;
 import com.ferreusveritas.dynamictrees.block.leaves.LeavesProperties;
 import com.ferreusveritas.dynamictrees.block.rooty.SoilProperties;
-import com.ferreusveritas.dynamictrees.init.DTClient;
 import com.ferreusveritas.dynamictrees.tree.family.Family;
 import com.ferreusveritas.dynamictrees.tree.species.Species;
-import net.minecraft.client.renderer.ItemBlockRenderTypes;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.core.Holder;
-import net.minecraft.data.worldgen.features.FeatureUtils;
+import maxhyper.dtquark.dynamictreesplus.PlusRegistries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
-import net.minecraftforge.registries.ForgeRegistries;
 import vazkii.quark.base.module.config.type.CompoundBiomeConfig;
 import vazkii.quark.content.world.config.BlossomTreeConfig;
 import vazkii.quark.content.world.module.BlossomTreesModule;
-import vazkii.quark.content.world.module.GlimmeringWealdModule;
-
-import java.util.LinkedList;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(DynamicTreesQuark.MOD_ID)
@@ -43,6 +31,10 @@ public class DynamicTreesQuark {
         modEventBus.addListener(this::clientSetup);
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::gatherData);
+
+        if (ModList.get().isLoaded("dynamictreesplus")){
+            modEventBus.register(PlusRegistries.class);
+        }
 
         MinecraftForge.EVENT_BUS.register(this);
 
